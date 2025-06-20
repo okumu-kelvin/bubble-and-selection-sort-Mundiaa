533d2bf42f49fdaef605d8ebe04cd136ddcca75b
Mundiaa <mundiamanuel5@gmail.com>
2025-06-20 10:57:40 +0300

    Update bubble_sort.py

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,18 +1,18 @@
 def bubble_sort(unsorted_list):
     # TODO: Implement bubble sort
-        n = len(unsorted_list)
-        for i in range(n):
+    n = len(unsorted_list)
+    for i in range(n):
             # Inner loop for comparisons and swaps within each pass
 
-            for j in range(0, n - i - 1):
-                # The -i-1 ensures that already sorted elements at the end are not re-compared
-                # Compare adjacent elements
-                if unsorted_list[j] > unsorted_list[j + 1]:
-                    # Swap if elements are in the wrong order
-                    unsorted_list[j], unsorted_list[j + 1] = unsorted_list[j + 1], unsorted_list[j]
+        for j in range(0, n - i - 1):
+            # The -i-1 ensures that already sorted elements at the end are not re-compared
+            # Compare adjacent elements
+            if unsorted_list[j] > unsorted_list[j + 1]:
+                # Swap if elements are in the wrong order
+                unsorted_list[j], unsorted_list[j + 1] = unsorted_list[j + 1], unsorted_list[j]
         return unsorted_list
 
-    # Example usage:
+# Example usage:
 my_list = [64, 34, 25, 12, 22, 11, 90]
 print("Original list:", my_list)
 sorted_list = bubble_sort(my_list)

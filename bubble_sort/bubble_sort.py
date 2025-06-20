c4e0e3bd09c3e5c636d7f6f980aad643caa53f8e
Mundiaa <mundiamanuel5@gmail.com>
2025-06-20 09:57:16 +0300

    Updated bubble_sort.py and Selection_sort.py

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,23 @@
 def bubble_sort(unsorted_list):
     # TODO: Implement bubble sort
-    pass
+        n = len(unsorted_list)
+        for i in range(n):
+            # Inner loop for comparisons and swaps within each pass
+
+            for j in range(0, n - i - 1):
+                # The -i-1 ensures that already sorted elements at the end are not re-compared
+                # Compare adjacent elements
+                if unsorted_list[j] > unsorted_list[j + 1]:
+                    # Swap if elements are in the wrong order
+                    unsorted_list[j], unsorted_list[j + 1] = unsorted_list[j + 1], unsorted_list[j]
+        return unsorted_list
+
+    # Example usage:
+my_list = [64, 34, 25, 12, 22, 11, 90]
+print("Original list:", my_list)
+sorted_list = bubble_sort(my_list)
+print("Sorted list (ascending):", sorted_list)
+
+#Expected output:
+#Original list: [64, 34, 25, 12, 22, 11, 90]
+#Sorted list (ascending): [11, 12, 22, 25, 34, 64, 90]
--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,25 @@
 def selection_sort(arr):
     # TODO: Implement selection sort
-    pass
+    n =len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j #Swap the found minimum element with the first element of the unsorted part
+                # This places the smallest element at its correct sorted position
+
+                arr[j], arr[min_index] = arr[min_index], arr[j]
+
+
+# Example usage:
+my_list = [64, 25, 12, 22, 11, 8]
+selection_sort(my_list)
+print("Sorted array: ", my_list)
+
+another_list = [5, 3, 8, 4, 2]
+selection_sort(another_list)
+print("Sorted array: ", another_list)
+
+#Expected Output:
+#Sorted array:  [64, 25, 12, 22, 11, 8]
+#Sorted array:  [5, 3, 8, 4, 2]
